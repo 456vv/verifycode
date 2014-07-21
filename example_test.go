883db5464210b560856e8a43789e33a44dcd797d
456vv <123@456vv.com>
2014-07-21 08:38:14 +0800

    Update example_test.go

--- example_test.go
+++ example_test.go
@@ -1,7 +1,7 @@
 package verifycode_test
 import (
     "os"
-    "color"
+    "image/color"
 )
 func ExampleNewVerifyCode(){
     var verifyCode = NewVerifyCode()
@@ -9,11 +9,12 @@ func ExampleNewVerifyCode(){
     verifyCode.SetColor([]color.Color{})
     verifyCode.SetBackground([]color.Color{color.RGBA{255, 255, 255, 255}})
     verifyCode.SetWidthWithHeight(500, 300)
-    verifyCode.SetFont([]string{"simsun.ttf"})
+    verifyCode.SetFont([]string{"C:\\Windows\\Fonts\\simsun.ttf"})
     verifyCode.SetFontSize(200)
     verifyCode.SetHinting(false)
     verifyCode.SetKerning(-100, 100)
     osFile, _ := os.Create("Verification code.png")
     defer osFile.Close()
-    verifyCode.PNG("abcd", osFile)
+    r := verifyCode.Rnd("1234567890qwertyuiopasdfghjklzxcvbnm", 4)
+    verifyCode.PNG(r, osFile)
 }
